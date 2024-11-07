4268ac7be469843ce49bba04d47e513f8afb76b8
장정안 <wjddks96@naver.com>
2024-11-07 21:36:34 +0900

    feat: 게임메세지 추가
    - 편의점 소개 말과 구매할 상품명 수량을 입력하는 게임메세지를 추가하였습니다.

---
+++ src/constants/gameMessages.js
@@ -0,0 +1,8 @@
+const GAME_MESSAGES = Object.freeze({
+  INTRODUCE_PRODUCT_LIST:
+    '안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n',
+  ENTER_PRODUCT_INFO:
+    '\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])\n',
+});
+
+export { GAME_MESSAGES };
