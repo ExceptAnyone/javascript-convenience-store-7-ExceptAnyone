486acdba32e2f417ed37e52127d40c1f87896554
장정안 <wjddks96@naver.com>
2024-11-11 05:16:59 +0900

    test: 멤버십서비스 단위 테스트 코드 작성

---
+++ __tests__/service/MembershipService.test.js
@@ -0,0 +1,20 @@
+import MembershipService from '../../src/service/MembershipService';
+
+describe('memberShipService', () => {
+  let membershipService;
+
+  beforeEach(() => {
+    membershipService = new MembershipService();
+  });
+
+  test('할인을 정확히 계산한다', () => {
+    expect(membershipService.calculateDiscount(6000)).toBe(1800);
+  });
+
+  test('최대 할인 금액을 초과하면 최대 할인 금액을 반환해야 한다', () => {
+    const amount = 100000;
+    const discount = membershipService.calculateDiscount(amount);
+
+    expect(discount).toBe(8000);
+  });
+});
