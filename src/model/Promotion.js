42eb5dd60d69c8b25d36f0df8fcfc53918e7b9f9
장정안 <wjddks96@naver.com>
2024-11-10 22:34:27 +0900

    feat: Promotion 클래스 구현
    - 프로모션 상품에 해당하는 상품 데이터와, 남은 재고를 계산하는 로직을 담고 있습니다.

---
+++ src/model/Promotion.js
@@ -0,0 +1,29 @@
+//@ts-check
+
+class Promotion {
+  constructor(name, buy, get, startDate, endDate) {
+    this.name = name;
+    this.buy = parseInt(buy);
+    this.get = parseInt(get);
+    this.startDate = new Date(startDate);
+    this.endDate = new Date(endDate);
+  }
+
+  isValid(currentDate = new Date()) {
+    return currentDate >= this.startDate && currentDate <= this.endDate;
+  }
+
+  calculateSetSize() {
+    return this.buy + this.get;
+  }
+
+  calculatePromotionSets(quantity) {
+    return Math.floor(quantity / this.calculateSetSize());
+  }
+
+  calculateRemainder(quantity) {
+    return quantity % this.calculateSetSize();
+  }
+}
+
+export default Promotion;
