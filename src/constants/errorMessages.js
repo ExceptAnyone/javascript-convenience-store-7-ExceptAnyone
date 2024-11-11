f5564cc32934c26c36c36ab5847f95945f07a523
장정안 <wjddks96@naver.com>
2024-11-11 14:38:42 +0900

    refactor: 미션 요구사항에 맞는 메세지들로 변경

--- src/constants/errorMessages.js
+++ src/constants/errorMessages.js
@@ -1,7 +1,7 @@
 const ERROR_MESSAGES = Object.freeze({
   EMPTY_INPUT: '[ERROR] 구매 정보가 입력되지 않았습니다. 다시 입력해 주세요.',
   INVALID_FORMAT:
-    '[ERROR] 구매 형식이 올바르지 않습니다. [상품명-수량] 형식으로 입력해 주세요.',
+    '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.',
   EMPTY_NAME: '[ERROR] 상품명이 입력되지 않았습니다. 다시 입력해 주세요.',
   EMPTY_QUANTITY: '[ERROR] 수량이 입력되지 않았습니다. 다시 입력해 주세요.',
   NOT_INTEGER: '[ERROR] 수량은 정수로 입력해 주세요. (예: 1, 2, 3)',
